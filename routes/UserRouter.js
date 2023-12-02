f2bd92b50335fdb1322bfc60f817037cf1cefbf5
partha2001ps <parthapn2017@gmail.com>
2023-12-02 17:04:57 +0530

    route

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -9,8 +9,8 @@ UserRouter.post('/singin', UserContorller.singIn);
 UserRouter.post('/reset-password', UserContorller.resetPassword);
 UserRouter.post('/reset-password/:OTP', UserContorller.newpassword);
 
-UserRouter.post('/', auth.auth_middleWare, urlcontroller.url_long)
 UserRouter.get('/', auth.auth_middleWare, urlcontroller.geturl)
+UserRouter.post('/', auth.auth_middleWare, urlcontroller.url_long)
 UserRouter.get('/:shortId', auth.auth_middleWare, urlcontroller.redirect_short);
 UserRouter.delete('/:shortId', auth.auth_middleWare, urlcontroller.delete_url);
 
